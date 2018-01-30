cb88aae70404ded8a88ab88dc0bb9252c371a919
Liane Hampe <liaham@xmera.de>
2018-01-30 15:03:56 +0000

    generates code_result.txt and security_results.txt

--- project_types/init.rb
+++ project_types/init.rb
@@ -24,23 +24,22 @@ Redmine::Plugin.register :project_types do
   name 'Project Types Plugin'
   author 'Liane Hampe'
   description 'This is a plugin for defining project types with individual project default settings.'
-  version '0.1.3'
+  version '0.2.0'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
 
-  requires_redmine :version_or_higher => '3.3.2'
-  
-  settings :default => {
-    
-  }, :partial => 'settings/project_types_settings'
-  
+  requires_redmine version_or_higher: '3.3.2'
+
+  settings default: {},
+           partial: 'settings/project_types_settings'
+
 #  project_module :project_types do
 #    permission :view_project_types, {:project_types => [:index, :show]}
 #    permission :manage_project_types, {:project_types => [:index, :show, :new, :create, :destroy]}
 #    permission :select_project_modules, {:project_types => :modules}, :require => :member
 #  end
-  
-  menu :admin_menu, :project_types, { :controller => 'project_types', :action => 'index' }, :caption => :label_project_type_plural, :html => {:class => 'icon icon-plugins'}
+
+  menu :admin_menu, :project_types, { controller: 'project_types', action: 'index' }, caption: :label_project_type_plural, html: { class: 'icon icon-plugins' }
 end
 
 # Adds the project types app/overrides directory to Rails'
