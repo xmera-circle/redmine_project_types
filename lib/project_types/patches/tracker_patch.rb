283f7ff37272a5043e0ac6feee1ba78f330d086b
Liane Hampe <liaham@xmera.de>
2019-02-17 11:27:17 +0000

    Fixes projects custom fields synchronization in tracker_patch.rb again.

--- lib/project_types/patches/tracker_patch.rb
+++ lib/project_types/patches/tracker_patch.rb
@@ -54,7 +54,7 @@ module ProjectTypes
           Project.all.each do |p|
             if p.tracker_ids.include?(self.id)
               # Update of project custom fields as long as the custom field is new for the project
-              p.issue_custom_field_ids << self.custom_field_ids unless (p.issue_custom_field_ids & self.custom_field_ids).empty?
+              p.issue_custom_field_ids << self.custom_field_ids if (p.issue_custom_field_ids & self.custom_field_ids).empty?
             end
           end if Project.any?
         end
