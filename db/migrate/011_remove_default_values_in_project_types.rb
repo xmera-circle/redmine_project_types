004e8ea8a3586840fc73cee909edda123e9155a2
Liane Hampe <liaham@xmera.de>
2021-03-16 09:18:22 +0100

    Clean up project types table
    The project types table has had redundant columns which are deleted:
    
    * is_public
    * default_member_role_id
    
    Implements #209

---
+++ db/migrate/011_remove_default_values_in_project_types.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+#
+# Redmine plugin for xmera called Project Types Plugin.
+#
+# Copyright (C) 2017-21 Liane Hampe <liaham@xmera.de>, xmera.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+class RemoveDefaultValuesInProjectTypes < ActiveRecord::Migration[4.2]
+  def self.up
+    remove_column :project_types, :is_public if column_exists?(:project_types, :is_public)
+    remove_column :project_types, :default_member_role_id if column_exists?(:project_types, :default_member_role_id)
+  end
+
+  def self.down
+    add_column :project_types, :is_public, :boolean, default: false, null: false unless column_exists?(:project_types, :is_public)
+    add_column :project_types, :default_member_role_id, :integer, foreign_key: true unless column_exists?(:project_types, :default_member_role_id)
+  end
+end
