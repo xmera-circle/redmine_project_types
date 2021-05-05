3d45243845ac613e45996329b0d1187fb318de0a
Liane Hampe <liaham@xmera.de>
2021-05-05 09:12:02 +0200

    Add nodata to project custom fields view
    If there are no project custom fields the user will get the nodata
    label.
    
    Fix also the stylesheet loading.

--- lib/project_types/hooks/view_layouts_base_html_head_hook_listener.rb
+++ lib/project_types/hooks/view_layouts_base_html_head_hook_listener.rb
@@ -19,7 +19,9 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-class ViewLayoutsBaseHtmlHeadHookListener < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_html_head,
-            partial: 'redmine_project_types/redmine_project_types_header_tags'
+module ProjectTypes
+  class ViewLayoutsBaseHtmlHeadHookListener < Redmine::Hook::ViewListener
+    render_on :view_layouts_base_html_head,
+              partial: 'redmine_project_types/redmine_project_types_header_tags'
+  end
 end
